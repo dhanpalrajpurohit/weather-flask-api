4c749e8e577511fd4ed4a211a32c7c755a224b6c
RAJPUROHIT DHANPAL SINGH <43945901+dhanpalrajpurohit@users.noreply.github.com>
2020-06-14 16:37:02 +0530

    Update checkWeather.py

--- checkWeather.py
+++ checkWeather.py
@@ -1,30 +1,10 @@
 from flask import Flask, render_template, request, flash
 import json
 import urllib.request
-from flask_sqlalchemy import SQLAlchemy
-import sqlite3
 import requests
 app = Flask(__name__)
 app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
 
-'''
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-db = SQLAlchemy(app)
-
-db.Model.metadata.reflect(db.engine)
-
-con = sqlite3.connect("weather.db")
-con.row_factory = sqlite3.Row
-cur = con.cursor()
-cur.execute("Select city form table")
-
-rows = cur.fetchall()
-
-class City(db.Model):
-  id= db.Column(db.Integer, primary_key= True)
-  name= db.Column(db.String(50),nullable = False)
-
-'''
 
 @app.route('/', methods=['POST','GET'])
 def weather():
@@ -32,7 +12,6 @@ def weather():
     if request.method == 'POST':
         city = request.form['city']
 
-    #api = 'ad06dc44bcfc93c5f3d1f48479679a37'
         source = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=ad06dc44bcfc93c5f3d1f48479679a37'
         list_of_data = requests.get(source).json()
         weather_data = []
